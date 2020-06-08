117b9ebd31d3adfe6ab9086a63e8d608daf6148c
Marek Romanski <marek.romanski@huuugegames.com>
2020-06-08 11:27:02 +0200

    added templates for homepage and about page

--- sailinglife/settings.py
+++ sailinglife/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sailinglife.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
